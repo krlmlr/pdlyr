852bccc0ea59011b9bc5e30695053c973c1a3935
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-11 18:20:51 +0200

    add compatibility functions

---
+++ R/compat.R
@@ -0,0 +1,21 @@
+#' @export
+mutate <- function(.data, ...) {
+  warning("Row names will be lost")
+  dplyr::mutate_(.data = .data, .dots = lazyeval::lazy_dots(...))
+}
+
+#' @export
+count <- function(df, vars = NULL, wt_var = NULL) {
+  .Deprecated("dplyr::count_", "plyr")
+  dplyr::count_(x = df, vars = vars, wt = wt_var, sort = FALSE) %>%
+    dplyr::rename(freq = n)
+}
+
+#' @export
+rename <- function(x, replace, warn_missing = TRUE) {
+  if (!warn_missing) {
+    warning("rename now always throws an error if names are not found.")
+  }
+  .Deprecated("dplyr::rename_", "plyr")
+  dplyr::rename_(.data = x, .dots = as.list(setNames(names(replace), unlist(replace))))
+}
