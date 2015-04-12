30a48ed8ca6d4eef7fc569e0b4eee10242c10dfb
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-12 16:56:51 +0200

    test full compatibility with plyr

--- tests/testthat/test-plyr.R
+++ tests/testthat/test-plyr.R
@@ -1,1 +1,14 @@
 context("plyr")
+
+with_compat <- function(compat, ...) {
+  with_mock(
+    `plyr::mutate` = compat$mutate,
+    `plyr::count` = compat$count,
+    `plyr::rename` = compat$rename,
+    force(...)
+  )
+}
+
+with_compat(plyr_compat,
+  testthat::test_package("plyr")
+)
