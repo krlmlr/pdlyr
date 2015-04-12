1237b4149c896f0d0e7a03735934eb5f6979f91d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-13 00:06:57 +0200

    lint

--- tests/testthat/test-plyr-warn.R
+++ tests/testthat/test-plyr-warn.R
@@ -8,7 +8,8 @@ call_compat <- function(compat, names = c("mutate", "count", "rename")) {
         name,
         mutate = compat$mutate(.data = mtcars, a = 0),
         count = compat$count(df = mtcars, vars = "cyl", wt_var = NULL),
-        rename = compat$rename(x = mtcars, list(mpg = "miles_per_gallon"), warn_missing = TRUE)
+        rename = compat$rename(x = mtcars, list(mpg = "miles_per_gallon"),
+                               warn_missing = TRUE)
       )
     }
   )
@@ -16,11 +17,15 @@ call_compat <- function(compat, names = c("mutate", "count", "rename")) {
 
 test_that("warnings are thrown", {
   expect_warning(call_compat(plyr_warn_compat), "^Semantics", all = TRUE)
-  expect_warning(call_compat(plyr_warn_compat, "mutate"), "Row names", all = FALSE)
-  expect_warning(call_compat(plyr_warn_compat, "count"), "Interface", all = FALSE)
-  expect_warning(call_compat(plyr_warn_compat, "rename"), "Entirely different interface", all = FALSE)
+  expect_warning(call_compat(plyr_warn_compat, "mutate"),
+                 "Row names", all = FALSE)
+  expect_warning(call_compat(plyr_warn_compat, "count"),
+                 "Interface", all = FALSE)
+  expect_warning(call_compat(plyr_warn_compat, "rename"),
+                 "Entirely different interface", all = FALSE)
 })
 
 test_that("otherwise identical to plyr", {
-  expect_equal(suppressWarnings(call_compat(plyr_warn_compat)), call_compat(plyr_compat))
+  expect_equal(suppressWarnings(call_compat(plyr_warn_compat)),
+               call_compat(plyr_compat))
 })
