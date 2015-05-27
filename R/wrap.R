d582958faa7508db4b8dcac33f9750e3b92ef8c6
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-05-27 11:42:57 +0200

    wrappers

---
+++ R/wrap.R
@@ -0,0 +1,33 @@
+#' Forwarders for overlapping functions in plyr and dplyr
+#'
+#' Of the overlapping functions in \code{plyr} and \code{dplyr},
+#' only three have a different interface and/or semantics:
+#' \code{count}, \code{mutate} and \code{rename}.
+#' These functions are forwarders to the corresponding functions
+#' in the \code{plyr} and \code{dplyr} packages.
+#' @name forward
+NULL
+
+#' @rdname forward
+#' @inheritParams plyr::count
+pcount <- plyr::count
+
+#' @rdname forward
+#' @inheritParams plyr::mutate
+pmutate <- plyr::mutate
+
+#' @rdname forward
+#' @inheritParams plyr::rename
+prename <- plyr::rename
+
+#' @rdname forward
+#' @inheritParams dplyr::count
+dcount <- dplyr::count
+
+#' @rdname forward
+#' @inheritParams dplyr::mutate
+dmutate <- dplyr::mutate
+
+#' @rdname forward
+#' @inheritParams dplyr::rename
+dmrename <- dplyr::rename
